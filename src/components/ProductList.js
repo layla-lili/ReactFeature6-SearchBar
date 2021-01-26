3b6505e89755b3b3e63b7f9a3aeafc98dbea8aa1
layla-lili <laylaisa.211@gmail.com>
2021-01-26 06:47:08 +0300

    Task Completed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,15 +5,18 @@ import ProductItem from "./ProductItem";
 import SearchBar from "./SearchBar";
 // Data
 import products from "../products";
+import React, { useState } from "react";
 
-const ProductList = () => {
-  const productList = products.map((product) => (
-    <ProductItem product={product} key={product.id} />
-  ));
+const ProductList = (props) => {
+  const [query, setQuery] = useState("");
+
+  const productList = products
+    .filter((product) => product.name.toLowerCase().includes(query))
+    .map((product) => <ProductItem product={product} key={product.id} />);
 
   return (
     <>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       <ListWrapper>{productList}</ListWrapper>
     </>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,22 @@ import { SearchBarStyled } from "../styles";
 const SearchBar = (props) => {
   return (
     <SearchBarStyled
-      onChange={(event) => console.log(event.target.value)}
-      placeholder="Search for a cookie name"
+      onChange={(event) => props.setQuery(event.target.value.toLowerCase())}
     />
   );
 };
+
+// const SearchBar = (props) => {
+//   const handleChange = (event) => {
+//     console.log(event.target.value);
+//   };
+//   return (
+//     <SearchBarStyled
+//       //onChange={(setQuery) => console.log(setQuery.event.target.value)}
+//       onChange={(setQuery) => handleChange(setQuery)}
+//       placeholder="Search for a cookie name"
+//     />
+//   );
+// };
 
 export default SearchBar;
